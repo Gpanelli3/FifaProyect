4bdb501220c0d50cc2794be524a1892ac10d5407
gpanelli3 <gpanelli3@gmail.com>
2025-02-04 19:00:37 -0300

    s

--- backend/main.py
+++ backend/main.py
@@ -3,9 +3,9 @@ from typing import Union
 from fastapi import FastAPI
 
 app = FastAPI()
+app.title="fifa analisis"
 
-
-@app.get("/")
+@app.get("/",tags=['home'])
 def read_root():
     return {"Hello": "World"}
 
@@ -13,3 +13,17 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+jugador1={
+    "id":1,
+    "nombre": "leo messi",
+    "media": 91
+
+}
+
+app.get("/jugador")
+
+
+@app.get("/productos/{id_prod}",tags=['pag2'])
+def productos(id_prod: int):
+    return{"el producto": id_prod}
