1c040eb64727c3ce78cee10a59af09a43ceb76a0
gpanelli3 <gpanelli3@gmail.com>
2025-02-06 14:57:52 -0300

    selects

--- backend/main.py
+++ backend/main.py
@@ -26,18 +26,31 @@ def read_item(item_id: int, q: Union[str, None] = None):
 def productos(id_prod: int):
     return{"el producto": id_prod}
 
-
+#1-listado de jugadores, paginado.
 @app.get("/jugadores")
-def mostar_jugadores():
+def jugadores():
+    with Session(engine) as session:
+        consulta=select(Jugador).limit(5).offset(2) #se puede hacer paginacion con esto
+        resultados=session.exec(consulta).all()
+        return resultados
+
+#2- Crear endpoint y pantalla que devuelva los detalles de un jugador específico, dado su ID e implementar algun grafico para mostrar sus skills (Hint: pueden utilizar
+numero : int =10
+@app.get("/jugadorEspecifico")
+def jugador_especifico():
     with Session(engine) as session:
         statement=select(Jugador.id, 
                          Jugador.long_name,
                          Jugador.age
-                         ).where (Jugador.long_name=="Cristiano Ronaldo dos Santos Aveiro")
+                         ).where (Jugador.id==numero)
         resultados=session.exec(statement).first()
         return {
             "id": resultados.id,
             "long_name": resultados.long_name,
             "age": resultados.age,
         }
+#3-Crear endpoint y pantalla que permita modificar la información de un jugado
+#4-Crear un endpoint que te permita crear un jugador,
+#5-Crear un Login para solo ver la info de forma autenticada
+
         
